46de4478b3c38fc665bbd6fe3d81c52a177f3118
Mark Pritchett <linuxcodeclub@gmail.com>
2020-10-01 12:21:55 +0100

    Initial Commit

---
+++ chaser/Assets/chaser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaser : MonoBehaviour
+{
+    public Transform targetTransform;
+    float speed = 7;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 displacementFromTarget = targetTransform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * speed;
+
+        float distanceToTarget = displacementFromTarget.magnitude;
+
+        if (distanceToTarget > 1.5f) {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+}
